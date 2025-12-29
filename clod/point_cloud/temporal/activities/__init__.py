b34d2e505a735a560324a556cefccd3981fcd45f
4ereshkin <cereskinm@gmail.com>
2025-12-29 20:01:38 +0300

    sas

--- clod/point_cloud/temporal/activities/__init__.py
+++ clod/point_cloud/temporal/activities/__init__.py
@@ -7,13 +7,27 @@ Temporal's activity execution model.  Activities should be idempotent
 whenever possible and return simple JSON‑serialisable results.
 """
 
-from .checkpoint_activities import las_choice
-from .checkpoint_activities import load_metadata_for_file
+from .gateway_activities import las_choice
+from .gateway_activities import load_metadata_for_file
 from .reproject_activities import reproject_file
 from .insert_activities import insert_file_into_db
 from .fetch_activities import fetch_file_from_db
 from .records_activities import read_records_table
-from .cesium_activities import convert_to_tileset
+# from .cesium_activities import convert_to_tileset
+from .pipe_activities import resolve_crs_to_pdal_srs
+from .export_activities import export_merged_laz
+from .ingest_activities import (
+    create_scan,
+    ensure_company,
+    ensure_crs,
+    ensure_dataset,
+    ensure_dataset_version,
+    upload_raw_artifact,
+    create_ingest_run,
+    process_ingest_run,
+    get_scan,
+    list_raw_artifacts,
+)
 
 __all__ = [
     "las_choice",
@@ -22,5 +36,16 @@ __all__ = [
     "insert_file_into_db",
     "fetch_file_from_db",
     "read_records_table",
-    "convert_to_tileset",
+    # "convert_to_tileset",
+    "create_scan",
+    "ensure_company",
+    "ensure_crs",
+    "ensure_dataset",
+    'ensure_dataset_version',
+    "upload_raw_artifact",
+    "create_ingest_run",
+    "process_ingest_run",
+    "get_scan",
+    "list_raw_artifacts",
+    'export_merged_laz'
 ]
