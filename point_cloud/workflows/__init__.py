0a9fb3ff9ac8b0cac747ee36a7e0b87f882341cd
4ereshkin <cereskinm@gmail.com>
2026-01-13 01:42:53 +0300

    preprocess_workflow.py продолжение

--- point_cloud/workflows/__init__.py
+++
@@ -1,9 +0,0 @@
-from .download_workflow import DownloadWorkflow, DownloadWorkflowParams
-from .profiling_workflow import ProfilingWorkflow, ProfilingWorkflowParams
-
-__all__ = [
-    "DownloadWorkflow",
-    "DownloadWorkflowParams",
-    "ProfilingWorkflow",
-    "ProfilingWorkflowParams",
-]
