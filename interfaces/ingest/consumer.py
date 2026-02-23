dd4afcf1b39f116ae5df29a54cb0d277e4ed2bab
4ereshkin <cereskinm@gmail.com>
2026-02-23 20:39:31 +0300

    cleaning when it'll end plz god save me

--- interfaces/ingest/consumer.py
+++ interfaces/ingest/consumer.py
@@ -1,1 +1,18 @@
+from aio_pika import IncomingMessage
 from application.ingest.use_case import StartIngestUseCase
+from interfaces.ingest.dto import IngestStartMessageDTO
+from interfaces.ingest.mappers import to_start_command
+
+
+class IngestConsumer:
+    def __init__(self, use_case: StartIngestUseCase):
+        self.use_case = use_case
+
+
+    async def process_message(self, message: IncomingMessage):
+        async with message.process():
+            dto = IngestStartMessageDTO.model_validate_json(message.body)
+            command = to_start_command(dto)
+
+            await self.use_case.execute(command)
+
